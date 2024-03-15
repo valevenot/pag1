ab46895fe3611c1cdad125ba96e0c1b633ccc5d2
valeria <valanvedel@gmail.com>
2024-03-15 17:10:34 -0400

    comment

---
+++ scrip.js
@@ -0,0 +1,60 @@
+
+const lista =[
+
+    "flips" ,
+    "cebolla" ,
+    "sopa" ,
+    "oreo" ,
+    "cereal" ,
+    
+]
+
+const usuario = {
+name: "valeria" ,
+age: "17" ,
+password: "1234" ,
+job: "frontend develop" ,
+}
+
+console.log ([usuario, usuario, usuario])
+
+// un String o cadena de texto se define como un conjunto de caracteres y para definirlos haremos uso de comillas 
+// dobles, simples o backstip
+
+// el tipo de dato numerico nos permiten guardar numeros enteros o decimales
+
+// un tipo de dato bolean solo tienen valores de verdadero y falso
+
+// el 0 funciona como "false" y el 1 como "true"
+
+// un Array es una lista de datos ordenados en memoria uno detras de otro a los cuales se les asigna un indice cuyo primer indice es 0
+// el ultimo indice de todo array es el -1
+
+// un objeto es un tipo de dato complejo que permite almacenar datos por medio de claves y valor
+
+// el dato undefined indefinido que espera obtener un valor por lo general este dato se obtiene cuando algo sale mal
+
+// el null es un tipo de dato vacio y esta vacio intencionalmente
+
+
+let variable = -0.1
+
+if (variable) {
+    console.log("se cumplio")
+} else {
+    console.log ("no se cumplio")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
